f8437474239508c16147998fa9ea51ffa1f7ffbd
rfumic <rokofumic@gmail.com>
2023-07-02 17:33:04 +0200

    Hard-coded images in html

--- main.js
+++ main.js
@@ -3,28 +3,6 @@ import btnLBlue from './assets/arrow-blue-left.png';
 import btnRBlue from './assets/arrow-blue-right.png';
 import btnLGray from './assets/arrow-gray-left.png';
 import btnRGray from './assets/arrow-gray-right.png';
-import image1 from './assets/slider-image-1.jpg';
-import image2 from './assets/slider-image-2.jpg';
-import image3 from './assets/slider-image-3.jpg';
-import image4 from './assets/slider-image-4.jpg';
-import image5 from './assets/slider-image-5.jpg';
-import image6 from './assets/slider-image-6.jpg';
-import image7 from './assets/slider-image-7.jpg';
-import image8 from './assets/slider-image-8.jpg';
-import image9 from './assets/slider-image-9.jpg';
-
-const imageRow1 = [
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
-];
-const imageRow2 = [...imageRow1].reverse();
 
 $(() => {
   // loading button images
@@ -53,15 +31,6 @@ $(() => {
       hoverButton('.button-right', btnRGray);
     }
   );
-
-  // attaching images to DOM
-  $.each(imageRow1, (i, src) => {
-    $('.row-top').append($('<img>').attr('src', src));
-  });
-
-  $.each(imageRow2, (i, src) => {
-    $('.bottom-image-container').append($('<img>').attr('src', src));
-  });
 
   let sliderActive = false;
 
